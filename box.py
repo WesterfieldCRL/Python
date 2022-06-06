122c391397b41b4e6f9ef26cb2f86f262c04fc9d
Wesley Anastasi <anastasicrl@gmail.com>
2022-06-06 08:40:58 -0700

    preparing for day one

---
+++ box.py
@@ -0,0 +1,9 @@
+from graphics import *
+
+win = GraphWin("Box", 500, 500)
+
+test = Rectangle(Point(250,0), Point(200,10))
+
+test.draw(win)
+
+win.getMouse()
---
+++ test.py
@@ -0,0 +1,9 @@
+print("Hello World")
+
+val = input("Enter your value: ")
+print(val)
+
+test = int(val)
+
+if val == 1:
+    print('success')
