a0bc40a047e5000f1b2bcc301ab7afcdb3a7581d
SirWesterfield <35206122+SirWesterfield@users.noreply.github.com>
2022-06-24 23:56:44 -0700

    changed test

--- test.py
+++ test.py
@@ -1,13 +1,9 @@
 from graphics import *
 
-window = GraphWin("Drawing Time", 800, 800)
+window = GraphWin("Window", 800, 800)
 
 box = Rectangle(Point(10,10),Point(300,300))
 
 box.draw(window)
-
-window.getMouse()
-
-box.move(100,0)
 
 window.getMouse()
